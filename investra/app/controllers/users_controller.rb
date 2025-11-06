c7db1c3cc422550a05c060b2ff45bdf35234239e
Hongyi Wu <hw3136@nyu.edu>
2025-11-06 16:33:56 -0500

    Add failing request spec for POST /users

--- investra/app/controllers/users_controller.rb
+++ investra/app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def new
-      @user = User.new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "Account created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
