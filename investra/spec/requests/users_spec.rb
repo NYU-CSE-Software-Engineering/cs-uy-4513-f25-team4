7e804da6b19512698a0b4cc24883a2489934598b
Hongyi Wu <hw3136@nyu.edu>
2025-11-06 16:14:25 -0500

    Add failing request spec for GET /signup

---
+++ investra/spec/requests/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /signup" do
+    it "renders the new template" do
+      get signup_path
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:new)
+    end
+  end
+end
