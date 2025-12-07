9ee79117abc5a6a317379ecbbe9948061ea06d7f
MMMMMaasd <zb2253@nyu.edu>
2025-12-08 03:52:32 +0800

    Fix News model specs

--- investra/spec/models/news_spec.rb
+++ investra/spec/models/news_spec.rb
@@ -4,13 +4,30 @@ RSpec.describe News, type: :model do
   let(:stock) { Stock.create!(symbol: 'AAPL', name: 'Apple Inc.', price: 150.00, available_quantity: 1000) }
   
   describe 'associations' do
-    it { should belong_to(:stock) }
+    it 'belongs to stock' do
+      news = News.new(stock: stock, title: 'Test', published_at: Time.current)
+      expect(news.stock).to eq(stock)
+    end
   end
   
   describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:stock) }
-    it { should validate_presence_of(:published_at) }
+    it 'requires a title' do
+      news = News.new(stock: stock, published_at: Time.current)
+      expect(news.valid?).to be false
+      expect(news.errors[:title]).to include("can't be blank")
+    end
+    
+    it 'requires a stock' do
+      news = News.new(title: 'Test', published_at: Time.current)
+      expect(news.valid?).to be false
+      expect(news.errors[:stock]).to include("must exist")
+    end
+    
+    it 'requires published_at' do
+      news = News.new(stock: stock, title: 'Test')
+      expect(news.valid?).to be false
+      expect(news.errors[:published_at]).to include("can't be blank")
+    end
   end
   
   describe '.recent_for_stock' do
